d37e69dd9fa4ad70a832911be56b3ce4b3650fca
Tech <tech@rootuw.edu>
2014-05-01 15:42:41 -0700

    Adding code and dependencies for facetaip_multiselect module

---
+++ srmods.facetapi.multiselect.js
@@ -0,0 +1,11 @@
+Drupal.behaviors.srmodsFacetApiMultiselectWidget = {
+  attach: function (context, settings) {
+    // Go through each facet API multiselect element that is being displayed.
+    jQuery('.facetapi-multiselect', context).each(function () {
+      // Attach the behavior to it.
+      jQuery(this).multiselect({
+        // Pass in whatever array of options you need here.
+      });
+    });
+  }
+};
